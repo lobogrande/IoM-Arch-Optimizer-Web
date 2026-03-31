2bf3ffc39778a46e8b95ffe53ad80104e063327a
jtietjen <jtietjen@illumina.com>
2026-03-31 07:07:17 -0700

    Add loadStateFromJson bulk update action to Zustand store

--- src/store.js
+++ src/store.js
@@ -18,6 +18,29 @@ const useStore = create((set) => ({
   setBaseStat: (stat, value) => set((state) => ({
     base_stats: { ...state.base_stats, [stat]: parseInt(value) || 0 }
   })),
+  
+  // Bulk load from JSON file
+  loadStateFromJson: (data) => set((state) => {
+    const newState = { ...state };
+    
+    // Parse Settings
+    if (data.settings) {
+      if (data.settings.asc1_unlocked !== undefined) newState.asc1_unlocked = data.settings.asc1_unlocked;
+      if (data.settings.asc2_unlocked !== undefined) newState.asc2_unlocked = data.settings.asc2_unlocked;
+      if (data.settings.arch_level !== undefined) newState.arch_level = data.settings.arch_level;
+      if (data.settings.current_max_floor !== undefined) newState.current_max_floor = data.settings.current_max_floor;
+      if (data.settings.hades_idol_level !== undefined) newState.hades_idol_level = data.settings.hades_idol_level;
+    }
+    
+    // Parse Base Stats
+    if (data.base_stats) {
+      newState.base_stats = { ...state.base_stats, ...data.base_stats };
+    }
+    
+    // (We will add upgrades and cards here later!)
+    
+    return newState;
+  }),
 }));
 
 export default useStore;
