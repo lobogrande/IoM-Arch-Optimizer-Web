251e9755ed8712547d92b3cde851fa5cb341eddf
jtietjen <jtietjen@illumina.com>
2026-05-19 17:19:02 -0700

    (chore): bump version to v2.1.0

--- src/ui_config.js
+++ src/ui_config.js
@@ -4,7 +4,7 @@
 // Adjust these numbers, hit Save, and watch your browser instantly update!
 // ==============================================================================
 
-export const APP_VERSION = "v2.0.2";
+export const APP_VERSION = "v2.1.0";
 
 // --- BASE STATS ---
 export const UI_STAT_IMG_WIDTH = 220;
